780c91e9c229360744df8c703d75e65e249631cf
Jörn Preuß <jp@rehub.team>
2023-03-10 16:42:39 +0100

    Add endpoint for vetos

--- src/database.py
+++ src/database.py
@@ -1,13 +1,12 @@
+from sqlalchemy.future import Engine  # for the type hint
 from sqlmodel import SQLModel, create_engine
 
-sqlite_url = "sqlite:///./test.db"
 
-engine = create_engine(sqlite_url, echo=True)
-
-
-def get_engine():
+def get_engine(db_name: str):
+    sqlite_url = f"sqlite:///./{db_name}.db"
+    engine = create_engine(sqlite_url, echo=True)
     return engine
 
 
-def init_db():
+def init_db(engine: Engine):
     SQLModel.metadata.create_all(engine)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlmodel import select  # type: ignore
 from sqlmodel import Session
 
-from database import engine
+from database import get_engine, init_db
 from models import SVProperty
 
 app = FastAPI()
+
+
+def get_main_engine():
+    engine = get_engine("production")
+    init_db(engine)
+    return engine
+
+
+def get_session():
+    with Session(get_main_engine()) as session:
+        yield session
 
 
 @app.get("/")
@@ -13,18 +25,45 @@ async def read_root():
 
 
 @app.get("/create/property/{name}")
-async def create_property_anonymously(name: str):
+async def create_property_anonymously(
+    *, session: Session = Depends(get_session), name: str
+):
     property = SVProperty(name=name)
-    with Session(engine) as session:
-        session.add(property)
-        session.commit()
+    session.add(property)
+    session.commit()
     return {"created": {"name": name}}
 
 
 @app.get("/user/{user}/create/property/{name}")
-async def user_create_property(user: str, name: str):
+async def user_create_property(
+    *, session: Session = Depends(get_session), user: str, name: str
+):
     property = SVProperty(name=name, created_by=user)
-    with Session(engine) as session:
-        session.add(property)
+
+    session.add(property)
+    session.commit()
+
+    session.refresh(property)
+    # TODO error handling
+    # return {"created": {"name": name}}
+    return {"created": property.dict()}
+
+
+@app.get("/user/{user}/veto/property/{name}")
+async def user_veto_property(
+    *, session: Session = Depends(get_session), user: str, name: str
+):
+
+    statement = select(SVProperty).where(SVProperty.name == name)
+    results = session.exec(statement)
+    property = results.one()
+
+    if property:
+        property.vetoed_by = property.vetoed_by + [user]
         session.commit()
-    return {"created": {"name": name}}
+
+        session.refresh(property)
+        # return {"vetoed": {"name": name}}
+        return {"vetoed": property.dict()}
+    else:
+        return {"error": "property not found"}
