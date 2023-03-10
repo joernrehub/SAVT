2fe8e985359c357a9bd0595148203d1ed293d5a0
Jörn Preuß <jp@rehub.team>
2023-03-10 17:45:53 +0100

    Move session fixture to conftest.py, rename test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,22 @@
 from datetime import datetime
 
 import pytest
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool
 
 
 @pytest.fixture(scope="function")
 def timestamp_str():
     return datetime.now().strftime(r"%Y-%m-%d %H:%M:%S:%f")
+
+
+@pytest.fixture(name="session")
+def session_fixture():
+    engine = create_engine(
+        "sqlite:///./pytest.db",
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+    )
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,21 +1,8 @@
 import pytest
 from fastapi.testclient import TestClient
-from sqlmodel import Session, SQLModel, create_engine
-from sqlmodel.pool import StaticPool
+from sqlmodel import Session
 
 from main import app, get_session
-
-
-@pytest.fixture(name="session")
-def session_fixture():
-    engine = create_engine(
-        "sqlite:///./pytest.db",
-        connect_args={"check_same_thread": False},
-        poolclass=StaticPool,
-    )
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        yield session
 
 
 @pytest.fixture(name="client")
