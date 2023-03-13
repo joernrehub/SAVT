dd96d95a0b0f51ffb2c5017953ce89e8b3b362f8
Jörn Preuß <jp@rehub.team>
2023-03-13 15:17:11 +0100

    Fix Relationship

--- src/models.py
+++ src/models.py
@@ -1,7 +1,8 @@
-from typing import Optional
+from typing import List, Optional
 
 from fastapi import Form
 from sqlmodel import Field  # type: ignore
+from sqlmodel import Relationship  # type: ignore
 from sqlmodel import JSON, Column, SQLModel
 
 # later
@@ -13,14 +14,31 @@ from sqlmodel import JSON, Column, SQLModel
 #     name: str
 
 
+# Use later for multiple objects (pizzas) with their own toppings
+class SVObject(SQLModel, table=True):
+    """An object with properties. Can be a pizza with toppings."""
+
+    # __tablename__ = "sv_objects"  # type: ignore
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    name: str
+    created_by: Optional[str] = None  # use SVUser later
+
+    properties: List["SVProperty"] = Relationship(back_populates="svobject")
+
+
 class SVProperty(SQLModel, table=True):
     """A property of an object. Can be a topping of a pizza."""
 
-    __tablename__ = "sv_properties"  # type: ignore
+    # __tablename__ = "sv_properties"  # type: ignore
 
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     created_by: Optional[str] = None  # use SVUser later
+
+    svobject_id: Optional[int] = Field(default=None, foreign_key="svobject.id")
+    svobject: Optional[SVObject] = Relationship(back_populates="properties")
 
     # to have a list of users who vetoed this property that works with sqlite
     vetoed_by: list[str] = Field(default=[], sa_column=Column(JSON))
@@ -35,21 +53,3 @@ class SVProperty(SQLModel, table=True):
         name: str = Form(...),
     ) -> "SVProperty":
         return cls(name=name)
-
-
-# Use later for multiple objects (pizzas) with their own toppings
-# does not work yet
-#
-# class SVObject(SQLModel, table=True):
-#     """An object with properties. Can be a pizza with toppings."""
-
-#     __tablename__ = "sv_objects"  # type: ignore
-
-#     id: Optional[int] = Field(default=None, primary_key=True)
-
-#     name: str
-#     created_by: Optional[str] = None  # use SVUser later
-
-#     properties: List[SVProperty] = Relationship(
-#         back_populates="sv_objects",
-#     )
