bb2385bebca79b624d0bd3991e31c3e0d025970b
Jörn Preuß <jp@rehub.team>
2023-03-14 10:02:32 +0100

    Add fastapi debug_toolbar

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 import sys
 from pathlib import Path
 
+from debug_toolbar.middleware import DebugToolbarMiddleware
 from fastapi import FastAPI
 
 sys.path.append(str(Path(__file__).parent))
 from api_routes import api_router
 from routes import router
 
-
-app = FastAPI()
+app = FastAPI(debug=True)
 app.include_router(api_router)
 app.include_router(router)
+app.add_middleware(
+    DebugToolbarMiddleware,
+    panels=["debug_toolbar.panels.sqlalchemy.SQLAlchemyPanel"],
+)
--- src/models.py
+++ src/models.py
@@ -14,24 +14,19 @@ from sqlmodel import JSON, Column, SQLModel
 #     name: str
 
 
-# Use later for multiple objects (pizzas) with their own toppings
 class SVObject(SQLModel, table=True):
     """An object with properties. Can be a pizza with toppings."""
 
     # __tablename__ = "sv_objects"  # type: ignore
 
     id: Optional[int] = Field(default=None, primary_key=True)
-
     name: str
     created_by: Optional[str] = None  # use SVUser later
 
     properties: List["SVProperty"] = Relationship(back_populates="object")
 
     @classmethod
-    def as_form(
-        cls,
-        name: str = Form(...),
-    ) -> "SVObject":
+    def as_form(cls, name: str = Form(...)) -> "SVObject":
         return cls(name=name)
 
 
@@ -44,11 +39,11 @@ class SVProperty(SQLModel, table=True):
     name: str
     created_by: Optional[str] = None  # use SVUser later
 
-    object_id: Optional[int] = Field(default=None, foreign_key="svobject.id")
-    object: Optional[SVObject] = Relationship(back_populates="properties")
-
     # to have a list of users who vetoed this property that works with sqlite
     vetoed_by: list[str] = Field(default=[], sa_column=Column(JSON))
+
+    object_id: Optional[int] = Field(default=None, foreign_key="svobject.id")
+    object: Optional[SVObject] = Relationship(back_populates="properties")
 
     # Needed for Column(JSON)
     class Config:  # type: ignore
@@ -56,9 +51,7 @@ class SVProperty(SQLModel, table=True):
 
     @classmethod
     def as_form(
-        cls,
-        name: str = Form(...),
-        object_id: Optional[Union[int, str]] = Form(None),
+        cls, name: str = Form(...), object_id: Optional[Union[int, str]] = Form(None)
     ) -> "SVProperty":
         if not object_id or not isinstance(object_id, int):
             return cls(name=name, object_id=None)
